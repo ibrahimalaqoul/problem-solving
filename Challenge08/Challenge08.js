c4e1d5527281dab827e58d5c8891751afb188090
ibrahimalaqoul <alaqoulibrahim@gmail.com>
2022-02-06 13:34:26 +0200

    solvedchal09

--- Challenge08/Challenge08.js
+++ Challenge08/Challenge08.js
@@ -27,7 +27,12 @@
 // -------------------------------------------------------------------------------------------------------
 
 const customerAndAge = (obj) => {
-  Array.obj
+    let myArray = [] ;
+    for ( let property in obj){
+        myArray.push(`Customer Name :${property} , Age :${obj[property]}`)
+    }
+    return myArray
+  
 };
 
 // -------------------------------------------------------------------------------------------------------
@@ -54,7 +59,10 @@ const customerAndAge = (obj) => {
 // -------------------------------------------------------------------------------------------------------
 
 const getEntries = (obj) => {
-  // write your code here
+  let myArray=[];
+  for (const [key, value] of Object.entries(obj)) {
+    myArray.push(`${key}: ${value}`);
+  } return myArray;
 };
 
 // -------------------------------------------------------------------------------------------------------
@@ -95,7 +103,12 @@ const courses = [
 const getInfo = (arr) => {
   let coursesName = [];
   let studentsName = [];
-  // write your code here
+  for (const courseArray of arr) {
+    coursesName.push(courseArray.course);
+    for (const stuArray of courseArray.Students) {
+      studentsName.push(stuArray)
+    }
+  }
 
   return { coursesName, studentsName };
 };
@@ -119,8 +132,23 @@ const getInfo = (arr) => {
 //  ------------------------------------------------------------------------------------------------------
 
 const getStudents = (arr) => {
-  // write your code here
+  let resultObj=[];
 
+for (const myArray of arr) {
+  for (const my2Array of courses) {
+    for (const my3Array of my2Array.Students ) {
+          if (myArray == my3Array) {
+            console.log(my3Array);
+        // resultObj.push(my3Array);
+        // resultObj.push(courses.course)
+        resultObj.push({Student:my3Array,course:my2Array.course})
+      } else console.log("error");
+    }
+    
+  } 
+}
+
+return resultObj;
 };
 
 module.exports = {
